08b1de8619673eff848ea427f8f014d523041b52
enteresanlikk <zeki1198@gmail.com>
2025-03-23 02:17:43 +0300

    remove get next and add get name.

--- nodes/common/base.go
+++ nodes/common/base.go
@@ -13,7 +13,6 @@ type Node interface {
 	Execute(inputs ...interface{}) []NodeOutput
 	SetNext(node Node)
 	GetName() string
-	GetNext() []Node
 }
 
 type BaseNode struct {
@@ -30,20 +29,18 @@ func (n *BaseNode) GetName() string {
 	return n.Name
 }
 
-func (n *BaseNode) GetNext() []Node {
-	return n.Next
-}
-
 func (n *BaseNode) ExecuteWithCheck(inputs []interface{}, execute func([]interface{}) interface{}) []NodeOutput {
-	fmt.Printf("Executing node: %s\n", n.Name)
+	name := n.GetName()
+
+	fmt.Printf("Executing node: %s\n", name)
 
 	if !n.Enabled {
-		fmt.Printf("%s Node is disabled, skipping...\n", n.Name)
-		return []NodeOutput{{NodeName: n.Name, Data: inputs[0]}}
+		fmt.Printf("%s Node is disabled, skipping...\n", name)
+		return []NodeOutput{{NodeName: name, Data: inputs[0]}}
 	}
 
 	result := execute(inputs)
-	outputs := []NodeOutput{{NodeName: n.Name, Data: result}}
+	outputs := []NodeOutput{{NodeName: name, Data: result}}
 
 	if len(n.Next) > 0 {
 		for _, nextNode := range n.Next {
